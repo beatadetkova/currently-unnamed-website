e0c30f30dea175d356a6e66d9dab100b8c5b83fb
Beata Detkova <beata.detkova.jr@gmail.com>
2020-10-16 17:15:08 +0200

    Implemented lazy loading

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Article.css';
 
-function Article(props) {
+function Article({ video }) {
+  const [videoSrc, setVideoSrc] = useState('');
+  const [articleRef, setArticleRef] = useState();
+
+  useEffect(() => {
+    let observer;
+    let didCancel = false;
+
+    if (articleRef && videoSrc !== video) {
+      if (IntersectionObserver) {
+        observer = new IntersectionObserver(
+          (entries) => {
+            entries.forEach((entry) => {
+              if (!didCancel && entry.isIntersecting) {
+                setVideoSrc(video);
+                observer.unobserve(articleRef);
+              }
+            });
+          },
+          {
+            threshold: 0.01,
+          }
+        );
+        observer.observe(articleRef);
+      } else {
+        setVideoSrc(video);
+      }
+    }
+    return () => {
+      didCancel = true;
+      if (observer && observer.unobserve) {
+        observer.unobserve(articleRef);
+      }
+    };
+  }, [video, videoSrc, articleRef]);
+
   return (
-    <div className="Article">
+    <div className="Article" ref={setArticleRef}>
       <iframe
         title="youtube-video"
         width="618"
         height="347.5"
-        src={props.video}
-        frameborder="0"
+        src={videoSrc}
+        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
+        allowFullScreen
       ></iframe>
     </div>
   );
