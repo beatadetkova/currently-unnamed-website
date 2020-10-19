9a03035b05544eaf9d46a40a214d2efa33ae3223
Beata Detkova <beata.detkova.jr@gmail.com>
2020-10-19 18:18:30 +0200

    Created Apply component and styling

---
+++ src/components/Apply/Apply.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Apply.css';
+
+function Apply() {
+  return (
+    <div id="application-form">
+      <iframe
+        title="Apply"
+        src="https://docs.google.com/forms/d/e/1FAIpQLSfTidi7OwYKxhcOY1kHwwInDZx2XfHFXK_DPwN5Iyt1aszn4w/viewform?embedded=true"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0"
+      >
+        Loading…
+      </iframe>
+    </div>
+  );
+}
+
+export default Apply;
