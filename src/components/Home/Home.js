ceb7d63529233458126ebad967ad441e7a5c6181
Beata Detkova <beata.detkova.jr@gmail.com>
2020-10-20 19:06:00 +0200

    Added text

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,8 +6,42 @@ function Home() {
   return (
     <div>
       <Intro />
-      <Article video="https://www.youtube.com/embed/Vdre3Tx10MA" />
-      <Article video="https://www.youtube.com/embed/HlCWhtmQPvo" />
+      <Article
+        video="https://www.youtube.com/embed/Vdre3Tx10MA"
+        title="Castle Nathria"
+      >
+        <p>Pull count:</p>
+        <ul>
+          <li>Shriekwing</li>
+          <li>Altimor the Huntsman: 1</li>
+          <li>Hungering Destroyer: 1</li>
+          <li>Artificer Xy'Mox: 1</li>
+          <li>Sun King's Salvation: 1</li>
+          <li>Lady Inerva Darkvein: 1</li>
+          <li>The Council of Blood: 1</li>
+          <li>Sludgefist: 1</li>
+          <li>Stoneborne Generals: 1</li>
+          <li>Sire Denathrius: 1</li>
+        </ul>
+      </Article>
+      <Article
+        video="https://www.youtube.com/embed/HlCWhtmQPvo"
+        title="Castle Nathria"
+      >
+        <p>Pull count:</p>
+        <ul>
+          <li>Shriekwing</li>
+          <li>Altimor the Huntsman: 1</li>
+          <li>Hungering Destroyer: 1</li>
+          <li>Artificer Xy'Mox: 1</li>
+          <li>Sun King's Salvation: 1</li>
+          <li>Lady Inerva Darkvein: 1</li>
+          <li>The Council of Blood: 1</li>
+          <li>Sludgefist: 1</li>
+          <li>Stoneborne Generals: 1</li>
+          <li>Sire Denathrius: 1</li>
+        </ul>
+      </Article>
     </div>
   );
 }
