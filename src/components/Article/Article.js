b2d4c826ebbaaaa7e322ab44ac08a3f74c129cb3
Beata Detkova <beata.detkova.jr@gmail.com>
2020-10-23 20:45:30 +0200

    Handling content smaller than its container

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -5,6 +5,7 @@ import Spinner from './Spinner.js';
 function Article({ video, children, title }) {
   const [videoSrc, setVideoSrc] = useState('');
   const [articleRef, setArticleRef] = useState();
+  const [contentRef, setContentRef] = useState();
   const [showVideo, setShowVideo] = useState(false);
   const [isExpanded, setIsExpanded] = useState(false);
   const [windowWidth, setwindowWidth] = useState(0);
@@ -86,10 +87,12 @@ function Article({ video, children, title }) {
         <h1>{title}</h1>
         <div onClick={expandContent}>
           <div id="text-content" className={isExpanded ? 'expanded' : ''}>
-            {children}
+            <div ref={setContentRef}>{children}</div>
           </div>
           <div id="expand-info">
-            {windowWidth < 600 ? `Show ${isExpanded ? 'less' : 'more'}` : ''}
+            {windowWidth < 600 && contentRef?.offsetHeight > 55
+              ? `Show ${isExpanded ? 'less' : 'more'}`
+              : ''}
           </div>
         </div>
       </div>
