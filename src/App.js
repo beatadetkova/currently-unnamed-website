50c9e71e59bcda3fea9212f9fb47443822242fba
Beata Detkova <beata.detkova.jr@gmail.com>
2020-10-29 13:16:00 +0100

    Changed BASE_NAME to PUBLIC_URL

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
-    <Router basename={process.env.BASE_NAME}>
+    <Router basename={process.env.PUBLIC_URL}>
       <div>
         <NavBar />
         <Switch>
